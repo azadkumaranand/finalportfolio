af2892b948e600afc09904f54fa3075e7828a04e
azadkumaranand <azadanand9798@gmail.com>
2023-03-22 13:40:03 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const navbar = document.querySelector('.navbar'),
+navbarspan = document.querySelector('.logo strong span')
+
+window.onscroll =  ()  =>{
+    // called when the window is scrolled.
+    if (window.scrollY >= 100) {
+        navbar.style.backgroundColor = "crimson"
+        navbarspan.style.color = "white";
+    }
+    else if(window.scrollY==0) {
+        navbar.style.backgroundColor = "transparent"
+        navbarspan.style.color = "crimson";
+    }
+}
+
+const menubar = document.getElementById('menubar');
+const menu = document.getElementById('menu');
+menubar.addEventListener('click', () => {
+    if (menu.style.display == "none") {
+        menu.style.display = "block";
+        menubar.innerHTML = `<img src="xmarks.png">`
+    }
+    else {
+        menu.style.display = "none"
+        menubar.innerHTML = `<img src="bars.png">`
+    }
+})
+const name = document.getElementById('name')
+function checkform(){
+    alert('Hey ' + name.value + ' Your form has been submitted')
+}
+var typed = new Typed(".typing", {
+    strings: ["YouTuber", "Desiner", "Freelacer", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+var typed = new Typed(".typing1", {
+    strings: ["YouTuber", "Desiner", "Freelacer", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
